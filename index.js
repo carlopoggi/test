14f3bd44761833f9095a34f6208eeaebcccc1e01
Carlo <carlo.poggi98@gmail.com>
2022-07-07 16:29:37 +0200

    "progressi"

---
+++ index.js
@@ -0,0 +1,37 @@
+var x = 0
+var b = 123
+var c = 12.765
+var d = 'pippo'
+var e = true // false
+var f = [1, 'pi', false]
+var g = {
+  nome: 'pruto',
+  eta: 3,
+  vivo: false,
+  numeri: [1, 5443, 0]
+}
+var h = null;
+var i = undefined;
+var l = function lol() {
+  alert('hahah');
+}
+
+function lol( ) {
+  console.log(' lol ')
+}
+
+const logLol = () => {
+  console.log(' lol ')
+}
+
+function a() {
+  return 2
+}
+
+const returnTwo = () => 2
+const returnTwoS = () => {
+  return 2
+}
+function returnTwoS() {
+  return 2
+}
